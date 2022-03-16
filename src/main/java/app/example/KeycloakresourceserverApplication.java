ad3bfb1d5ed08ed1084021b9006f26cb5114989e
vijay <vijay.srj@gmail.com>
2022-03-16 19:11:41 +0530

    initial push

---
+++ src/main/java/app/example/KeycloakresourceserverApplication.java
@@ -0,0 +1,13 @@
+package app.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakresourceserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakresourceserverApplication.class, args);
+	}
+
+}
---
+++ src/main/java/app/example/TestController.java
@@ -0,0 +1,15 @@
+package app.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	
+	@GetMapping("/test")
+	public String test() {
+		
+		return "success";
+	}
+}
